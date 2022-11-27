df826de6beaf05d84c71219167ab140dd651e32b
unknown <stl.12061201@gmail.com>
2022-11-27 23:06:35 +0200

    add1

---
+++ 26.09.2022/Task 9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+int main() {
+int n1, n2, n3, n4;
+  cin >> n1;
+  cin >> n2;
+  cin >> n3;
+  cin >> n4;
+  cout << n1 << "." << n2 << "." << n3 << "." << n4;
+}
